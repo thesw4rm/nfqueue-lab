83c763f328ac30cfc087cf807be1be68e9aa8795
Yadunandan Pillai <thesw4rm@pm.me>
2019-09-13 04:27:37 -0400

    Fixed stuff for client

--- iptables_rules.sh
+++ iptables_rules.sh
@@ -1,6 +1,6 @@
 # SYN Packet DEST NAT
-iptables -t mangle -I PREROUTING -p tcp --dport 8000 --tcp-flags SYN,RST,ACK,FIN SYN -j NFQUEUE --queue-num 0 --queue-bypass
-
+iptables -t mangle -I OUTPUT -p tcp --dport 8000 --tcp-flags SYN,RST,ACK,FIN SYN -j NFQUEUE --queue-num 0 --queue-bypass
+iptables -t mangle -I POSTROUTING -s 10.0.8.4 -p tcp --dport 8000 -j SNAT --to-source 10.10.70.135:8000
 
 
 #iptables -t mangle -I PREROUTING -p tcp --sport 8000 --tcp-flags SYN,RST,ACK,FIN SYN,ACK -j NFQUEUE --queue-num 0 --queue-bypass # Get SYNACK handshake packet
--- upload_script.sh
+++ upload_script.sh
@@ -1,1 +1,1 @@
-rsync -ru  --info=progress2 --info=name0  /home/ytpillai/documents/clubs/cybersec/hands_on_labs/advanced_linux_09_13/nfqueue_server root@csec-adv-linux-server:/root/
+rsync -ru  --info=progress2 --info=name0  /home/ytpillai/documents/clubs/cybersec/hands_on_labs/advanced_linux_09_13/nfqueue_client root@csec-adv-linux-client:/root/
