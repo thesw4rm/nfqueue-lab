1aa9ebd01f291ab0d34456e1c4c3b7b58a47ffce
Yadunandan Pillai <thesw4rm@pm.me>
2019-09-13 04:06:05 -0400

    Fixed iptables rules

--- iptables_rules.sh
+++ iptables_rules.sh
@@ -1,17 +1,9 @@
 # SYN Packet DEST NAT
 iptables -t mangle -I PREROUTING -p tcp --dport 8000 --tcp-flags SYN,RST,ACK,FIN SYN -j NFQUEUE --queue-num 0 --queue-bypass
 
-# SYN Packet SRC NAT
-iptables -t mangle -I POSTROUTING -p tcp --dport 8000 --tcp-flags SYN,RST,ACK,FIN SYN -j NFQUEUE --queue-num 1 --queue-bypass
 
-# SYNACK Packet DEST NAT
-iptables -t mangle -I PREROUTING -p tcp --sport 8000 --tcp-flags SYN,RST,ACK,FIN SYN,ACK -j NFQUEUE --queue-num 0 --queue-bypass
-
-# SYNACK PACKET SRC NAT
-iptables -t mangle -I POSTROUTING -p tcp --sport 8000 --tcp-flags SYN,RST,ACK,FIN SYN,ACK -j NFQUEUE --queue-num 1 --queue-bypass
 
 #iptables -t mangle -I PREROUTING -p tcp --sport 8000 --tcp-flags SYN,RST,ACK,FIN SYN,ACK -j NFQUEUE --queue-num 0 --queue-bypass # Get SYNACK handshake packet
-iptables -t mangle -I PREROUTING -p tcp --sport 8000 --tcp-flags SYN,RST,ACK,FIN SYN,ACK -j LOG --log-prefix "SYNACK_PACKET_DETECTED: PREROUTING" --log-level 4
 #iptables -t mangle -A FORWARD -p tcp --dport 8000 --tcp-flags SYN,RST,ACK,FIN SYN -j MARK --set-mark 145
 
 #iptables -t mangle -I FORWARD -p tcp --dport 8000 --tcp-flags SYN,RST,ACK,FIN SYN -j LOG --log-prefix "SYN_PACKET_DETECTED: FORWARD" --log-level 4
